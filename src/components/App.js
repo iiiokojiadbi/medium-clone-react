4f0aeaec99dbf51612e1f4024dae3fdb1bf6714c
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-30 21:59:57 +0300

    feat: использовать контекст

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import Routes from 'routes';
 import {BrowserRouter} from 'react-router-dom';
-import {Header} from 'components';
 import 'blocks/app.scss';
 import 'blocks/main.scss';
 
-export const App= () => {
+import {Header} from 'components';
+import {CurrentUserProvider} from 'context/CurrentUserContext';
+
+export const App = () => {
   return (
-    <>
+    <CurrentUserProvider>
       <BrowserRouter>
         <Header />
         <div className='main'>
           <Routes />
         </div>
       </BrowserRouter>
-    </>
+    </CurrentUserProvider>
   );
 };
