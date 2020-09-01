13c41e8b118dc19f7258d603ae545466ca3c8f9e
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-09-01 16:37:20 +0300

    feat: реализовать запрос инфо юзера по токену

--- src/components/CurrentUserChecker.js
+++ src/components/CurrentUserChecker.js
@@ -1,21 +1,47 @@
-import React, {useEffect, useContext} from 'react';
+import {useEffect, useContext} from 'react';
 import useFetch from 'hooks/useFetch';
+import useLocalStorage from 'hooks/useLocalStorage';
+
 import {CurrentUserContext} from 'context/CurrentUserContext';
 
 export const CurrentUserChecker = ({children}) => {
-  const [currentUserState, setCurrentUserState] = useContext(
-    CurrentUserContext
-  );
+  const [, setCurrentUserState] = useContext(CurrentUserContext);
+  const [token] = useLocalStorage('token');
   const [{response}, doFetch] = useFetch('/user');
 
   useEffect(() => {
-    console.log('init');
-    doFetch();
+    if (!token) {
+      setCurrentUserState((prevState) => ({
+        ...prevState,
+        isLoggedIn: false,
+      }));
+      return;
+    }
+
+    doFetch({
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Token ${token}`,
+      },
+    });
     setCurrentUserState((prevState) => ({
       ...prevState,
       isLoading: true,
     }));
-  }, []);
+  }, [token, setCurrentUserState, doFetch]);
+
+  useEffect(() => {
+    if (!response) {
+      return;
+    }
+    setCurrentUserState((prevState) => ({
+      ...prevState,
+      isLoggedIn: true,
+      isLoading: false,
+      currentUser: response.user,
+    }));
+  }, [response, setCurrentUserState]);
 
   return children;
 };
