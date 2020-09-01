167e6ac327f14386514568a2e09f2f3da0a83d91
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-09-01 14:17:24 +0300

    feat: выполнить проверку статуса в хуке фетча

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -12,13 +12,26 @@ export default (url) => {
     setIsLoading(true);
   };
 
+  // const returnResponse = (response) => {
+  //   if(response.ok) {
+  //     return response.json();
+  //   }
+
+  //   return Promise.reject(``)
+  // }
+
   useEffect(() => {
     if (isLoading) {
       fetch(baseUrl + url, options)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          setResponse(data);
+        .then((response) =>
+          response.json().then((data) => ({status: response.status, data}))
+        )
+        .then(({status, data}) => {
+          if (status === 200) {
+            setResponse(data);
+          } else {
+            setError(data);
+          }
           setIsLoading(false);
         })
         .catch((err) => {
