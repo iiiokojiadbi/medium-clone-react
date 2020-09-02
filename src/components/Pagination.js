6a5cb85fe7c4e45922aab00dc6872e0152aaf99c
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-09-02 21:53:55 +0300

    feat: добавить новый компонент

---
+++ src/components/Pagination.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Pagination = ({total, limit, url, currentPage}) => {
+  return <div>Pagination</div>;
+};
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import {Header} from 'components/Header';
 import {TopBar} from 'components/TopBar';
 import {CurrentUserChecker} from 'components/CurrentUserChecker';
 import {Feed} from 'components/Feed';
+import {Pagination} from './Pagination';
 
-export {App, Header, TopBar, CurrentUserChecker, Feed};
+export {App, Header, TopBar, CurrentUserChecker, Feed, Pagination};
