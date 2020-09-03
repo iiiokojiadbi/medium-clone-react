0dcfcc689292efd50da2c7433c7a721c7182104e
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-09-03 17:38:04 +0300

    fix: убрать бесконечный скролл

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -6,17 +6,20 @@ export const TagList = ({tags, optionalClasses}) => {
   const listClasses = classNames({
     'tag-list': true,
     [optionalClasses]: optionalClasses,
+    'tag-list_block': !tags.length,
   });
 
   console.log(listClasses);
 
   return (
     <ul className={listClasses}>
-      {tags.map((tag) => (
-        <li key={tag} className='tag-list__item-list'>
-          {tag}
-        </li>
-      ))}
+      {tags.map((tag) => {
+        return (
+          <li key={tag} className='tag-list__item-list'>
+            {tag}
+          </li>
+        );
+      })}
     </ul>
   );
 };
